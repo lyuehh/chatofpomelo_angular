afb4abc37fc7e840beb5adddea8513ea12ef64f7
lyuehh <lyuehh@gmail.com>
2013-11-21 17:11:13 +0800

    now private talk work

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -51,6 +51,31 @@ function AppCtrl($scope) {
     //wait message from the server.
     pomelo.on('onChat', function(data) {
         console.log('[client][onChat] data: ' + JSON.stringify(data));
+        var conversation = _.filter($scope.conversations, function(c) {
+            return (c.from === data.from && c.to === data.to) ||
+                (c.from === data.to && c.to === data.from);
+        });
+        if (name === data.from) {
+            return;
+        }
+        if (conversation.length === 0) {
+            var c = {
+                type: 'private', // 私聊，2个人
+                from: data.to,
+                to: data.from,
+                messages: [{
+                    user: data.from,
+                    message: data.message
+                }]
+            };
+            $scope.conversations.push(c);
+        } else {
+            conversation[0].messages.push({
+                user: data.from,
+                message: data.message
+            });
+        }
+        $scope.$apply();
         //addMessage(data.from, data.target, data.msg);
         //$("#chatHistory").show();
         //if(data.from !== username)
@@ -86,100 +111,45 @@ function AppCtrl($scope) {
     $scope.messages = [];
     $scope.conversations = [];
 
-    // 私聊
-    $scope.talkto = function(socketid, name) {
+    // 私聊, 弹出消息框
+    $scope.talkto = function(name) {
         var conversation = {
             type: 'private', // 私聊，2个人
-            to: {
-                id: socketid,
-                name: name
-            },
-            from: {
-                id: $scope.user.socketid,
-                name: $scope.user.username
-            },
+            to: name,
+            from: $scope.user,
             messages: []
         };
         $scope.conversations.push(conversation);
-        console.log($scope.conversations);
+    };
 
+    // 发送 消息
+    $scope.sendMessage = function(from, to, message, type) {
+        console.log('[client][sendMessage] from : ' + from + ', to: ' + to + ', message: ' + message);
+        var cid;
+        if (type === 'private') {
+            cid = from + '_' + to;
+        } else if (type === 'group') {
+            cid = to;
+        } else if (type === 'domain') {
+            cid = 'domain'; // example.com
+        }
         pomelo.request("chat.chatHandler.send", {
-            rid: rid,
-            content: msg,
-            from: username,
-            target: target
+            type: type,
+            cid: cid,
+            message: message,
+            from: from,
+            to: to
         }, function(data) {
-            //$("#entry").attr("value", ""); // clear the entry field.
-            //if(target != '*' && target != username) {
-            //    addMessage(username, target, msg);
-            //    $("#chatHistory").show();
-            //}
+            console.log('[client][chatHandler][send] got: ' + JSON.stringify(data));
         });
-    };
 
-    // 发送 私聊消息
-    $scope.sendMessage = function(from, to, message) {
-        console.log('[client][sendMessage] from : ' + JSON.stringify(from) + ', to: ' + JSON.stringify(to) + ', message: ' + message);
-
-        socket.emit('send:message', {
-            message: message,
-            from: from,
-            to: to
-        });
         var conversation = _.filter($scope.conversations, function(c) {
-            return (c.from.id === from.id && c.to.id === to.id) ||
-                (c.from.id === to.id && c.to.id === from.id);
+            return (c.from === from && c.to === to) ||
+                (c.from === to && c.to === from);
         });
         conversation[0].messages.push({
-            user: from.name,
-            text: message
-        });
-        $scope.message = '';
-    };
-
-    // 群组聊天
-    $scope.talktoGroup = function (group, name) {
-        var conversation = {
-            type: 'group', // 群组，多个人
-            to: {
-                id: group.id,
-                name: group.name
-            },
-            from: {
-                id: $scope.user.socketid,
-                name: $scope.user.username
-            },
-            messages: []
-        };
-        $scope.conversations.push(conversation);
-        console.log($scope.conversations);
-        socket.emit('group:join',  {
-            group: {
-                id: group.id,
-                name: group.name
-            },
-            user: {
-                id: $scope.user.socketid,
-                name: $scope.user.username
-            }
-        });
-    };
-
-    // 发送消息给 群组
-    $scope.sendMessageToGroup = function(from, to, message) {
-        socket.emit('send:group', {
-            message: message,
-            from: from,
-            to: to
-        });
-        console.log('[client][sendMessageToGroup] from: ' + JSON.stringify(from) + ',t o: ' + JSON.stringify(to) + ', message: ' + message);
-        var conversation = _.filter($scope.conversations, function(c) {
-            return (c.type === 'group' && c.to.name === to.name);
-        });
-        console.log('[sendMessageToGroup] conversation: ' + JSON.stringify(conversation));
-        conversation[0].messages.push({
-            user: from.name,
-            text: message
+            user: from,
+            message: message
         });
         $scope.message = '';
     };
